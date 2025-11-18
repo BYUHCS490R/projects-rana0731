9c8ae432b1b461724bd237fe59fea18c47cbd4a6
Anar Fuxiang <anar.fuxiang@gmail.com>
2025-11-17 18:16:26 -1000

    js and json

---
+++ projects/form/form.js
@@ -0,0 +1,41 @@
+document.getElementById('myForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    const fullname = document.getElementById("fullname").value;
+    const email = document.getElementById('email').value;
+    const age = document.getElementById('age').value;
+    const password = document.getElementById('pass').value;
+            
+
+    if (!fullname || email) {
+        alert("You need a name and email.");
+    }
+
+    if(!age || age < 18){
+        alert("You need to be 18");
+    }
+
+    const formData= {
+        name: fullname,
+        email: email,
+        password: password,
+        age: age
+    };
+
+    console.log(formData);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4 && xhr.status === 200){
+            alert("Form submitted succesfully!");
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+            // document.getElementById('myForm').reset;
+            document.getElementById('myForm').innerHTML = '';
+            document.getElementById('message').innerText = response.message;
+        }else if(xhr.readyState === 4){
+            alert("Error submitting form.");
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+});
